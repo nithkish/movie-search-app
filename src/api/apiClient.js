a223581681398e83a46bfaf892dc00adfca8ee4c
Nithin Kishore <nithin.kishore.j@volkswagen.co.in>
2022-10-14 22:25:50 +0530

    added constant file

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {BASE_URL} from "../constants/api"
 
 export const axiosClient = axios.create({
-  baseURL: `https://swapi.dev/api/`,
+  baseURL: BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
